e34be29574421a4088409776691f336b3d946088
CamMcDB <cam.mcb@gmail.com>
2013-10-08 16:40:08 +0100

    Update someCode.c

--- someCode.c
+++ someCode.c
@@ -4,7 +4,12 @@ class someShittyCode {
   ShittyFunction
   for (int cnt=0; cnt<100; cnt++)
   {
-    if (cnt%3)
+    if (cnt%3 = 0)
     printf ("Fizzzzz")
+  }
+  for (int c=0; c<100; c++)
+  {
+    if(c%5 = 0)
+    printf ("Buzzzzzzzzzz")
   }
 }
